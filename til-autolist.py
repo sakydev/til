c7f47930cc4c84472a55df1db5019075ec40ccb5
Saqib Razzaq <saqibrzzaq@gmail.com>
2021-05-16 16:46:52 +0500

    When automatically updating README, print file count as well

--- til-autolist.py
+++ til-autolist.py
@@ -3,17 +3,24 @@
 from os import listdir, path
 
 output = 'Things I Learned Today about programming languages, frameworks and libararies or technology in general  \n\n'
+total = 0
 for mainFile in listdir('.'):
 	directory = filepath = ''
 	if path.isdir(mainFile):
 		output += '\n**' + mainFile.capitalize() + '**  \n'
 		directory += mainFile
+		current = 0
 		for file in listdir(mainFile):
 			if file.endswith('.md'):
 				filepath = directory + '/' + file
 				filetitle = file.replace('-', ' ').replace('.md', '').capitalize()
 				output += f'- [{filetitle}]({filepath})  \n'
+				current += 1
+				total += 1
 
+		print(f'{mainFile} = {current} files')
+
+print(f'\n{total} total TILs so far\n')
 file = open('README.md', 'w')
 file.write(output)
 file.close()
